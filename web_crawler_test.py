66c45bf374b1ab8358b8d43b96e4a27ba84784a5
Mantagar <mantagar@proton.me>
2025-08-17 16:49:11 +0200

    Switch tabs to 2 spaces

--- web_crawler_test.py
+++ web_crawler_test.py
@@ -2,28 +2,28 @@ from web_crawler import Crawler, UrlExtractor
 import pytest
 
 class MockHtmlProvider():
-	LINK_TEMPLATE = '<a href="{}">link</a>'
-	def __init__(self, url_map):
-		self.url_to_html_map = {url:self.build_html(url_map[url]) for url in url_map}
+  LINK_TEMPLATE = '<a href="{}">link</a>'
+  def __init__(self, url_map):
+    self.url_to_html_map = {url:self.build_html(url_map[url]) for url in url_map}
 
-	def fetch_html(self, url):
-		return self.url_to_html_map[url]
+  def fetch_html(self, url):
+    return self.url_to_html_map[url]
 
-	def build_html(self, hrefs):
-		html = ""
-		for i in hrefs:
-			html += MockHtmlProvider.LINK_TEMPLATE.format(i)	
-		return html
+  def build_html(self, hrefs):
+    html = ""
+    for i in hrefs:
+      html += MockHtmlProvider.LINK_TEMPLATE.format(i)  
+    return html
 
 test_url_1 = "http://test.url"
 test_url_2 = "http://test.url/sth"
 test_url_3 = "https://another-test.url/sth"
 test_url_4 = "https://depth-test.url"
 url_test_mapping = {
-	test_url_1: (test_url_2,),
-	test_url_2: (test_url_3,),
-	test_url_3: (),
-	test_url_4: (test_url_1, test_url_2)
+  test_url_1: (test_url_2,),
+  test_url_2: (test_url_3,),
+  test_url_3: (),
+  test_url_4: (test_url_1, test_url_2)
 }
 
 # mock downloading pages
@@ -36,7 +36,7 @@ Therefore I used fixtures to get a fresh instance every time.
 """
 @pytest.fixture
 def crawler():
-	return Crawler(url_extractor, html_provider)
+  return Crawler(url_extractor, html_provider)
 
 """
 This test is for a fringe scenario when an url was first
@@ -44,31 +44,31 @@ seen at depth 0 (not crawled) and the seen again with
 higher depth. Such an url is expected not to be skipped.
 """
 def test_seen_urls_on_depth_0(crawler):
-	max_depth = 2
-	crawler.crawl(test_url_4, max_depth)
-	assert len(crawler.found_urls) == 4
-	assert test_url_3 in crawler.found_urls
+  max_depth = 2
+  crawler.crawl(test_url_4, max_depth)
+  assert len(crawler.found_urls) == 4
+  assert test_url_3 in crawler.found_urls
 
 def test_depth_0(crawler):
-	max_depth = 0
-	crawler.crawl(test_url_1, max_depth)
-	assert len(crawler.found_urls) == 1
-	assert test_url_1 in crawler.found_urls
+  max_depth = 0
+  crawler.crawl(test_url_1, max_depth)
+  assert len(crawler.found_urls) == 1
+  assert test_url_1 in crawler.found_urls
 
 def test_depth_1(crawler):
-	max_depth = 1
-	crawler.crawl(test_url_1, max_depth)
-	assert len(crawler.found_urls) == 2
-	assert test_url_1 in crawler.found_urls
-	assert test_url_2 in crawler.found_urls
+  max_depth = 1
+  crawler.crawl(test_url_1, max_depth)
+  assert len(crawler.found_urls) == 2
+  assert test_url_1 in crawler.found_urls
+  assert test_url_2 in crawler.found_urls
 
 def test_depth_2(crawler):
-	max_depth = 2
-	crawler.crawl(test_url_1, max_depth)
-	assert len(crawler.found_urls) == 3
-	assert test_url_1 in crawler.found_urls
-	assert test_url_2 in crawler.found_urls
-	assert test_url_3 in crawler.found_urls
+  max_depth = 2
+  crawler.crawl(test_url_1, max_depth)
+  assert len(crawler.found_urls) == 3
+  assert test_url_1 in crawler.found_urls
+  assert test_url_2 in crawler.found_urls
+  assert test_url_3 in crawler.found_urls
 
 """
 NOTE: this test is great, it made me realize there was a severe bug
@@ -78,16 +78,16 @@ the urls extracted from the html at hand, all previously seen urls were crawled 
 Adding another test just to handle such a case: test_stateless_aggregate
 """
 def test_depth_3(crawler):
-	max_depth = 3
-	crawler.crawl(test_url_3, max_depth)
-	assert len(crawler.found_urls) == 1
-	assert test_url_3 in crawler.found_urls
+  max_depth = 3
+  crawler.crawl(test_url_3, max_depth)
+  assert len(crawler.found_urls) == 1
+  assert test_url_3 in crawler.found_urls
 
 def test_stateless_aggregate():
-	max_depth = 1
-	crawler = Crawler(url_extractor, html_provider)
-	crawler.crawl(test_url_1, max_depth)
-	assert len(crawler.found_urls) == 2
-	crawler = Crawler(url_extractor, html_provider)
-	crawler.crawl(test_url_3, max_depth)
-	assert len(crawler.found_urls) == 1
+  max_depth = 1
+  crawler = Crawler(url_extractor, html_provider)
+  crawler.crawl(test_url_1, max_depth)
+  assert len(crawler.found_urls) == 2
+  crawler = Crawler(url_extractor, html_provider)
+  crawler.crawl(test_url_3, max_depth)
+  assert len(crawler.found_urls) == 1
